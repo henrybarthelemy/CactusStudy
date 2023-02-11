8017e97c66ed4d00d338bf86431227b44bd827ab
Lucas Sta Maria <lucas@priime.dev>
2023-02-10 23:41:06 -0500

    fix: format and change some text
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 import {
   Flex,
@@ -10,46 +10,49 @@ import {
   Button,
   Icon,
   IconProps,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
 export default function App() {
   return (
-    <Container maxW={'5xl'}>
+    <Container maxW={"5xl"}>
       <Stack
-        textAlign={'center'}
-        align={'center'}
+        textAlign={"center"}
+        align={"center"}
         spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}>
+        py={{ base: 20, md: 28 }}
+      >
         <Heading
           fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}>
-          Meeting scheduling{' '}
-          <Text as={'span'} color={'orange.400'}>
+          fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+          lineHeight={"110%"}
+        >
+          Studying flashcards{" "}
+          <Text as={"span"} color={"orange.400"}>
             made easy
           </Text>
         </Heading>
-        <Text color={'gray.500'} maxW={'3xl'}>
+        <Text color={"gray.500"} maxW={"3xl"}>
           Never miss a meeting. Never be late for one too. Keep track of your
           meetings and receive smart reminders in appropriate times. Read your
           smart “Daily Agenda” every morning.
         </Text>
-        <Stack spacing={6} direction={'row'}>
+        <Stack spacing={6} direction={"row"}>
           <Button
-            rounded={'full'}
+            rounded={"full"}
             px={6}
-            colorScheme={'orange'}
-            bg={'orange.400'}
-            _hover={{ bg: 'orange.500' }}>
-            Get started
+            colorScheme={"orange"}
+            bg={"orange.400"}
+            _hover={{ bg: "orange.500" }}
+          >
+            Sign up
           </Button>
-          <Button rounded={'full'} px={6}>
-            Learn more
+          <Button rounded={"full"} px={6}>
+            Log in
           </Button>
         </Stack>
-        <Flex w={'full'}>
+        <Flex w={"full"}>
           <Illustration
-            height={{ sm: '24rem', lg: '28rem' }}
+            height={{ sm: "24rem", lg: "28rem" }}
             mt={{ base: 12, sm: 16 }}
           />
         </Flex>
@@ -65,7 +68,8 @@ export const Illustration = (props: IconProps) => {
       viewBox="0 0 702 448"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}>
+      {...props}
+    >
       <path
         fillRule="evenodd"
         clipRule="evenodd"
@@ -832,7 +836,8 @@ export const Illustration = (props: IconProps) => {
           y1="345.577"
           x2="527.144"
           y2="147.032"
-          gradientUnits="userSpaceOnUse">
+          gradientUnits="userSpaceOnUse"
+        >
           <stop stopColor="#B8D8D5" />
           <stop offset="0.42" stopColor="#CEE0DA" />
           <stop offset="1" stopColor="#FFF0E6" />
@@ -843,7 +848,8 @@ export const Illustration = (props: IconProps) => {
           y1="106.847"
           x2="-86.7512"
           y2="315.127"
-          gradientUnits="userSpaceOnUse">
+          gradientUnits="userSpaceOnUse"
+        >
           <stop stopColor="#B8D8D5" />
           <stop offset="0.47" stopColor="#CCDFDA" />
           <stop offset="1" stopColor="#FFF0E6" />
@@ -854,7 +860,8 @@ export const Illustration = (props: IconProps) => {
           y1="150.967"
           x2="671.263"
           y2="250.917"
-          gradientUnits="userSpaceOnUse">
+          gradientUnits="userSpaceOnUse"
+        >
           <stop stopColor="#FFBF2F" />
           <stop offset="1" stopColor="#FD9500" />
         </linearGradient>
@@ -864,7 +871,8 @@ export const Illustration = (props: IconProps) => {
           y1="337.91"
           x2="471.34"
           y2="225.092"
-          gradientUnits="userSpaceOnUse">
+          gradientUnits="userSpaceOnUse"
+        >
           <stop stopColor="#FFBF2F" />
           <stop offset="0.48" stopColor="#FEB422" />
           <stop offset="1" stopColor="#FD9500" />
